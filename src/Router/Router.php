4cbc245472f16dede2368366052fb2af5f2e2478
Steven Oyer <75387453+stevenoyer@users.noreply.github.com>
2023-01-27 10:55:50 +0100

    Revert "Router modification"

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -6,10 +6,7 @@ use Exception;
 
 class Router
 {
-    private array $routes = [];
-    private array $matches = [];
-    private string $namespace_controllers = '\So\Blog\Controller\\';
-    private mixed $action;
+    private array $routes;
 
     /**
      * This function allows you to retrieve a route
@@ -18,93 +15,9 @@ class Router
      * 
      * @return void
      */
-    public function post(string $path, callable|string $action)
+    public function get(string $path, callable|string $action): void
     {
-        return $this->add($path, $action, 'POST');
-        $this->routes['POST'][$path] = $action;
-    }
-
-    /**
-     * This function allows you to retrieve a route
-     * @param string $path
-     * @param callable|string $action
-     * 
-     * @return void
-     */
-    public function get(string $path, callable|string $action)
-    {
-        return $this->add($path, $action, 'GET');
-        $this->routes['GET'][$path] = $action;
-    }
-
-    /**
-     * Function that adds a new route to the routes
-     * @param string $path
-     * @param callable|string $action
-     * @param string $method
-     * 
-     * @return callable|string
-     */
-    public function add(string $path, callable|string $action, string $method): callable|string
-    {
-        return $this->routes[$method][$path] = $action;
-    }
-
-    /**
-     * Function that matches a route with the URL, if they match, then true is returned otherwise false
-     * @param string $path
-     * @param string $uri
-     * 
-     * @return bool
-     */
-    public function match(string $path, string $uri): bool
-    {
-        $path = preg_replace('#:([\w]+)#', '([^/]+)', $path);
-        $regex = "#^$path$#i";
-
-        if (!preg_match($regex, $uri, $matches))
-        {
-            return false;
-        }
-
-        array_shift($matches);
-        $this->matches = $matches;
-        return true;
-    }
-
-    public function call()
-    {
-        // If callable, return method
-        if (is_callable($this->action))
-        {
-            return call_user_func_array($this->action, $this->matches);
-        }
-
-        // If string, treatment of the controller and the method
-        if (is_string($this->action))
-        {
-            /**
-             * Explode the string, example : HomeController@index
-             * HomeController = Controller
-             * index = method 
-             */
-            $explode = explode('@', $this->action);
-            $controller = $explode[0];
-            $method = $explode[1];
-
-            // Variable setting of the controller with its namespace
-            $className = $this->namespace_controllers . $controller;
-
-            // If the class and method exist, we instantiate the class and call the method
-            if (class_exists($className) && method_exists($className, $method))
-            {
-                $class = new $className();
-                $class->name = strtolower(str_replace('Controller', '', $controller));
-                return call_user_func_array([$class, $method], $this->matches);
-            }
-        }
-
-        return false;
+        $this->routes[$path] = $action;
     }
 
     /**
@@ -113,17 +26,41 @@ class Router
      */
     public function run(string $uri): mixed
     {
-        $this->action = false;
+        $path = '/' . explode('?', rtrim($uri))[0];
+        $action = $this->routes[$path] ?? null;
 
-        // We loop on all routes
-        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $path => $route)
+        // If callable, return method
+        if (is_callable($action))
         {
-            // Checks if the url matches a route
-            if ($this->match($path, $uri))
+            return $action();
+        }
+
+        // If string, treatment of the controller and the method
+        if (is_string($action))
+        {
+            /**
+             * Explode the string, example : HomeController@index
+             * HomeController = Controller
+             * index = method 
+             */
+            $explode = explode('@', $action);
+            $controller = $explode[0];
+            $method = $explode[1];
+
+            // Calling the namespace
+            $namespace_controllers = '\So\Blog\Controller\\';
+
+            // Variable setting of the controller with its namespace
+            $className = $namespace_controllers . $controller;
+
+            // If the class and method exist, we instantiate the class and call the method
+            if (class_exists($className) && method_exists($className, $method))
             {
-                // Implementation of the action
-                $this->action = $this->routes[$_SERVER['REQUEST_METHOD']][$path] ?? null;
-                echo $this->call();
+                $class = new $className();
+                $class->name = strtolower(str_replace('Controller', '', $controller));
+                $response = call_user_func([$class, $method]);
+
+                echo $response;
                 return true;
             }
         }
