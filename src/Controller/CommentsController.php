43291c07a0d727fb9b3d033965e372a459b0ad62
Steven Oyer <contact.stevenoyer@gmail.com>
2023-02-11 01:18:03 +0100

    Implementation of comment management (CRUD)

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -2,14 +2,19 @@
 
 namespace So\Blog\Controller;
 
+use So\Blog\Auth\Auth;
 use So\Blog\Class\Controller;
 use So\Blog\HTML\FormValidatorHtml;
 use So\Blog\Security\CsrfToken;
 
 class CommentsController extends Controller
 {
-    public function create(string $slug): string
+    /**
+     * Process create a new comment of an article by its slug
+     */
+    public function create(string $slug): string|bool
     {
+        $auth = new Auth();
         $id = (int) explode('-', $slug, 2)[0];
 
         if (empty($id))
@@ -28,7 +33,7 @@ class CommentsController extends Controller
             return $this->redirect(BASEURL . '/article/' . $slug);
         }
 
-        if (empty($_SESSION['id']))
+        if (!$auth->isConnected())
         {
             return $this->redirect(BASEURL . '/auth');
         }
@@ -39,6 +44,94 @@ class CommentsController extends Controller
 
         $comment = ['content' => $data['content'], 'author' => $_SESSION['id'], 'post_id' => $id, 'validation' => 0];
         if ($model->create($comment))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        return $this->redirect(BASEURL . '/article/' . $slug);
+    }
+
+    /**
+     * Process update comment by slug article and comment id
+     */
+    public function update(string $slug, int $id_comment): string|bool
+    {
+        $auth = new Auth();
+        $model = $this->getModel();
+        $id = (int) explode('-', $slug, 2)[0];
+
+        if (empty($id))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (empty($id_comment))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (empty($_POST['token']))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (!$this->csrf->verif($_POST['token']))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (!$auth->isConnected())
+        {
+            return $this->redirect(BASEURL . '/auth');
+        }
+
+        $validator = new FormValidatorHtml($_POST);
+        $data = $validator->validate();
+
+        $comment = ['content' => $data['content'], 'validation' => 0];
+        if ($model->update($id_comment, $comment))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        return $this->redirect(BASEURL . '/article/' . $slug);
+    }
+
+    /**
+     * Process delete comment by slug article and comment id
+     */
+    public function delete(string $slug, int $id_comment): string|bool
+    {
+        $auth = new Auth();
+        $model = $this->getModel();
+        $id = (int) explode('-', $slug, 2)[0];
+
+        if (empty($id))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (empty($id_comment))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (empty($_POST['token']))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (!$this->csrf->verif($_POST['token']))
+        {
+            return $this->redirect(BASEURL . '/article/' . $slug);
+        }
+
+        if (!$auth->isConnected())
+        {
+            return $this->redirect(BASEURL . '/auth');
+        }
+        
+        if ($model->delete($id_comment))
         {
             return $this->redirect(BASEURL . '/article/' . $slug);
         }
--- src/Model/CommentsModel.php
+++ src/Model/CommentsModel.php
@@ -9,10 +9,30 @@ class CommentsModel extends Model
     protected $table = 'comments';
 
     /**
-     * @param int $id
-     * @return array
+     * Read comments items
      */
-    public function findCommentByPostId(int $id): array|object|bool
+    public function read(int $limit = 5): array|object|bool
+    {
+        $limit_sql = '';
+        if ($limit != 0)
+        {
+            $limit_sql = "LIMIT $limit";
+        }
+
+        return $this->query("
+            SELECT c.*, u.name AS author_name, p.title as post_title
+            FROM $this->table AS c
+            LEFT JOIN users AS u ON u.id = c.author
+            LEFT JOIN posts AS p ON p.id = c.post_id
+            ORDER BY c.created_at DESC
+            $limit_sql
+        ");
+    }
+
+    /**
+     * Find comments by post id
+     */
+    public function findCommentsByPostId(int $id): mixed
     {
         return $this->query("
             SELECT c.*, u.name AS author_name
@@ -21,6 +41,20 @@ class CommentsModel extends Model
             WHERE c.post_id = ? AND validation = ?
             ORDER BY c.created_at DESC
         ", [$id, 1], false);
+    }
+
+    /**
+     * Find comments by user id
+     */
+    public function findCommentsByUserId(int $id): mixed
+    {
+        return $this->query("
+            SELECT c.*, p.title as post_title
+            FROM $this->table AS c
+            LEFT JOIN posts AS p ON p.id = c.post_id
+            WHERE c.author = ?
+            ORDER BY c.update_at DESC;
+        ", [$id], false);
     }
 
 }
